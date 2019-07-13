9c0a152e1ac041d5780a860b42aa3b54e91fef7c
Benjamin Mark <mquadrant@gmail.com>
2019-07-13 18:26:41 -0400

    created intent for aboutALC activity

--- app/src/main/java/com/gigaconsults/alc4_phase1/MainActivity.java
+++ app/src/main/java/com/gigaconsults/alc4_phase1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.gigaconsults.alc4_phase1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button alcWebBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alcWebBtn = findViewById(R.id.about_alc_btn);
+
+        alcWebBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AboutALC.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
